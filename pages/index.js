70fb9d0cae20722a8797403ff94d86b345e23d71
shivank <shivankkunwar100@gmail.com>
2022-07-08 23:43:00 +0530

    next installed tailwind

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+   <div className='bg-red-600'>
+    <h1>SHIVANK IS WORKING HERE
+      </h1>
+      </div>
+  )
+}
